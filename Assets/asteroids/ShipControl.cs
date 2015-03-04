bd09bee7a9170ce04c8b76c194e7eb33e985ade4
unknown <mark.schreckengost@MSCHRECK-PC.corp.1010data.com>
2015-03-04 17:14:05 -0500

    wip asteroids control scheme

---
+++ Assets/asteroids/ShipControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 ff = new Vector2(Input.GetAxis ("Horizontal"),0f);
+		GetComponent<Rigidbody>().AddForceAtPosition(
+			0.1f*(GetComponent<Transform>().rotation*ff),
+		    GetComponent<Transform>().rotation*(new Vector2(0f,1f)),
+		    ForceMode.Impulse);
+		Debug.Log (ff);
+	}
+}
