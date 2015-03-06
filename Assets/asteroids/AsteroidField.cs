1d73c8e4d6b50687a9da3ea088d424d3b92fc0af
schreck <schreckengost@gmail.com>
2015-03-06 12:15:27 -0500

    punted on elastic cam, added some background asteroids

---
+++ Assets/asteroids/AsteroidField.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidField : MonoBehaviour {
+
+	public Transform asteroid_prefab;
+	public Transform cam; // main camera
+
+	// Use this for initialization
+	void Start () {
+		int i;
+		int j;
+		Transform astro;
+		float astro_size;
+		for(i=0;i<20;i++){
+			for(j=0;j<20;j++){
+				astro = GameObject.Instantiate(asteroid_prefab,30*new Vector3(i,j,0),Random.rotation) as Transform;
+				astro.parent = GetComponent<Transform>();
+				astro_size = 10*Random.value;
+				astro.localScale = new Vector3(astro_size,astro_size,astro_size);
+				astro.GetComponent<Rigidbody>().mass = astro_size;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/asteroids/FollowCam.cs
+++ Assets/asteroids/FollowCam.cs
@@ -4,23 +4,35 @@ using System.Collections;
 public class FollowCam : MonoBehaviour {
 
 	public Transform to_follow;
+	public float jerk_mag = 1f; // magnitude of jerk (m/sss)
 
 	private Camera cam;
 	private Transform tx;
 	private float target_size;
-	private Vector3 target_position; // dampped ??
+	private Vector3 target_pos; 
+	private Vector3 target_vel; 
+
+	// motion state
+	private Vector3 vel; // velocity (m/s)
+	private Vector3 acc; // acceleration (m/ss)
 
 	// Use this for initialization
 	void Start () {
 		cam = GetComponent<Camera>();
 		tx = GetComponent<Transform>();
+		vel = Vector3.zero;
+		acc = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		target_position = to_follow.position;
-		target_position.z = -10; // dont think it matters
+		target_pos = to_follow.position;
+		target_pos.z = -10; // dont think it matters
+		//target_vel = to_follow.GetComponent<Rigidbody>().velocity;
+ 		// punt on the cam intercept stuff
+		tx.position = target_pos;
 		target_size = 5+to_follow.GetComponent<Rigidbody>().velocity.magnitude;
-		//tx.position = (target_position-tx.position)
+		cam.orthographicSize+=(target_size-cam.orthographicSize)/2;
+
 	}
 }
